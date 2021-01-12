0a1ccbdcaf3f823880e62fdbf68716b2083fd7ec
TonyHomsi <tonyhomsi87@gmail.com>
2021-01-12 09:35:07 +0100

    types of lacators

--- cypress/integration/firstTest.spec.js
+++ cypress/integration/firstTest.spec.js
@@ -1,23 +1,42 @@
-
+/// <reference types="cypress" />
 
 describe('Our first suite', ()=> {
-
-    describe('Our suite section', ()=> {
-
-        beforeEach('code for every test', ()=>{
-            // repectitive code
-        })
-    })
-
     it('first test', ()=> {
 
-    })
+        cy.visit('/')
 
-    it('second test', ()=> {
-        
-    })
+        cy.contains('Forms').click()
+        cy.contains('Form Layouts').click()
 
-    it('third test', ()=> {
-        
+        // by Tag Name
+        cy.get('input')
+
+        // by ID
+        cy.get('#inputEmail1')
+
+        // by Class Name
+        cy.get('.input-full-width')
+
+        // by Attribute name
+        cy.get('[placeholder]')
+
+        // by Attribute name and value
+        cy.get('[placeholder="Email"]')
+
+        //by Class value
+        cy.get('[class="input-full-width size-medium shape-rectangle"]')
+
+        // by Tag name and Attribute with value
+        cy.get('input[placeholder="Email"]')
+
+        // by two different attributes
+        cy.get('[placeholder="Email"][type="email"]')
+
+        // by Tag name, Attribute with value, ID and Class name
+        cy.get('input[placeholder="Email"]#inputEmail1.input-full-width')
+
+        // The most recommended way by Cypress
+        cy.get('[data-cy="imputEmail1"]')
+
     })
 })
