74f3d76f35b3f7a22dd7febb74b33e7a7e223358
TonyHomsi <tonyhomsi87@gmail.com>
2021-01-12 08:46:13 +0100

    tests structure

---
+++ cypress/integration/firstTest.spec.js
@@ -0,0 +1,23 @@
+
+
+describe('Our first suite', ()=> {
+
+    describe('Our suite section', ()=> {
+
+        beforeEach('code for every test', ()=>{
+            // repectitive code
+        })
+    })
+
+    it('first test', ()=> {
+
+    })
+
+    it('second test', ()=> {
+        
+    })
+
+    it('third test', ()=> {
+        
+    })
+})
